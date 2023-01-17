e58660af27f910b1c49d40fa773fa3d5676ed49a
normalisahalim <122872100+normalisahalim@users.noreply.github.com>
2023-01-17 17:05:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,85 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+#import libraries 
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
 
-"""
-# Welcome to Streamlit!
+df = pd.read_csv('data.csv') 
+df.shape
+df.isna().sum()
+df = df.dropna(axis=1)
+df.shape
+df['diagnosis'].value_counts()
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+from sklearn.preprocessing import LabelEncoder
+labelencoder_Y = LabelEncoder()
+df.iloc[:,1]= labelencoder_Y.fit_transform(df.iloc[:,1].values)
+print(labelencoder_Y.fit_transform(df.iloc[:,1].values))
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+df.head(5)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+X = df.iloc[:, 2:31].values 
+Y = df.iloc[:, 1].values
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+def models(X_train,Y_train):
+  
+  #Using Logistic Regression 
+  from sklearn.linear_model import LogisticRegression
+  log = LogisticRegression(random_state = 0)
+  log.fit(X_train, Y_train)
+  
+  #Using KNeighborsClassifier 
+  from sklearn.neighbors import KNeighborsClassifier
+  knn = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
+  knn.fit(X_train, Y_train)
+    
+  #print model accuracy on the training data.
+  print('[0]Genetic Fuzzy System Accuracy:', log.score(X_train, Y_train))
+  print('[1]Neural Network Accuracy:', knn.score(X_train, Y_train))
 
-    points_per_turn = total_points / num_turns
+return log, knn
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+model = models(X_train,Y_train)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+from sklearn.metrics import confusion_matrix
+for i in range(len(model)):
+  cm = confusion_matrix(Y_test, model[i].predict(X_test))
+  
+  TN = cm[0][0]
+  TP = cm[1][1]
+  FN = cm[1][0]
+  FP = cm[0][1]
+  
+  print(cm)
+  print('Model[{}] Testing Accuracy = "{}!"'.format(i,  (TP + TN) / (TP + TN + FN + FP)))
+  print()
+    
+  from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+
+for i in range(len(model)):
+  print('Model ',i)
+  #Check precision, recall, f1-score
+  print( classification_report(Y_test, model[i].predict(X_test)) )
+  #Another way to get the models accuracy on the test data
+  print( accuracy_score(Y_test, model[i].predict(X_test)))
+  print()#Print a new line
+    
+#Print Prediction of Random Forest Classifier model
+pred = model[6].predict(X_test)
+print(pred)
+
+#Print a space
+print()
+
+#Print the actual values
+print(Y_test)
